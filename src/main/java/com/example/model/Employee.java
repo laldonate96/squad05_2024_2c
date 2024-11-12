33d8927f805053270a2598c4b853b1206e1e00ea
laldonate96 <lucasaldonate@gmail.com>
2024-11-12 17:19:56 -0300

    feat: add example create/get for employees

--- src/main/java/com/example/model/Employee.java
+++ src/main/java/com/example/model/Employee.java
@@ -1,4 +1,27 @@
 package com.example.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public Employee() {}
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
